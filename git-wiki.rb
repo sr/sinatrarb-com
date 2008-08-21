44441224d68ae94e29bddb285c561dc4b6e6bb43
Simon Rozet <simon@rozet.name>
2008-08-21 22:32:28 +0200

    fix last commit

--- git-wiki.rb
+++ git-wiki.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-$: << File.expand_path(*Dir[File.dirname(__FILE__) + '/vendor/**/lib'].to_a)
+$: << *Dir[File.dirname(__FILE__) + '/vendor/**/lib'].to_a
 %w(sinatra
 rubygems
 git

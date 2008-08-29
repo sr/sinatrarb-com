b08593d8d663b8686156857c6163d5ab0c2ab549
Simon Rozet <simon@rozet.name>
2008-08-29 15:21:32 +0200

    extract core_ext

---
+++ core_ext.rb
@@ -0,0 +1,40 @@
+class String
+  def to_html
+    BlueCloth.new(self).to_html.linkify
+  end
+
+  def linkify
+    self.gsub(/([A-Z][a-z]+[A-Z][A-Za-z0-9]+)/) do |page|
+      %Q{<a class="" href="/#{Page.css_class_for(page)}">#{page.titleize}</a>}
+    end
+  end
+
+  def titleize
+    self.gsub(/([A-Z]+)([A-Z][a-z])/,'\1 \2').gsub(/([a-z\d])([A-Z])/,'\1 \2')
+  end
+
+  def without_ext
+    self.sub(File.extname(self), '')
+  end
+end
+
+class Time
+  def distance_in_words_from_now(from_time = 0)
+    from_time = from_time.to_time if from_time.respond_to?(:to_time)
+    distance_in_minutes = (((self - Time.now).abs)/60).round
+
+    case distance_in_minutes
+      when 0..1
+        return (distance_in_minutes == 0) ? 'less than a minute' : '1 minute'
+      when 2..44           then "#{distance_in_minutes} minutes"
+      when 45..89          then 'about 1 hour'
+      when 90..1439        then "about #{(distance_in_minutes.to_f / 60.0).round} hours"
+      when 1440..2879      then '1 day'
+      when 2880..43199     then "#{(distance_in_minutes / 1440).round} days"
+      when 43200..86399    then 'about 1 month'
+      when 86400..525599   then "#{(distance_in_minutes / 43200).round} months"
+      when 525600..1051199 then 'about 1 year'
+      else                      "over #{(distance_in_minutes / 525600).round} years"
+    end
+  end
+end
